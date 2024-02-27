e21866402b58a43a275053cbea15bba05fcc5a42
sergio chica <sergioanjaci@gmail.com>
2024-02-27 14:27:54 -0500

    Update Perfile and Fixed Menu

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -13,7 +13,29 @@ export const Home = () => {
     <Main>
       <section className='firstSection'>
       <TitleCover />
-      <p></p>
+      <div className='containerFirstSection'>
+      <div className='TextFirstSection'>
+      <p className='textOne'>Soy un estudiante resolutivo y proactivo que le gusta aprender cada vez más sobre el mundo del software, enfocandome en ser Fullstack para tener mas conocimientos sobre la mayoria de campos que abarque la industria del software.</p>
+      <p className='textSecond'>ACTUALMENTE ESTOY REALIZANDO UN TECNOLOGO SOBRE DESARROLLO DE SOFTWARE Y A LA MISMA VEZ APRENDIENDO MAS MEDIANTE CURSOS.</p>
+      <div className='textThreeAndTextFour'>
+        <p className='textThree'>Si quieres ponerte en contacto conmigo escribeme en</p>
+        <p className='textFour'>sergioanjaci@gmail.com</p>
+      </div>
+      </div>
+      </div>
+      </section>
+      <section className='firstSection'>
+      <TitleCover />
+      <div className='containerFirstSection'>
+      <div className='TextFirstSection'>
+      <p className='textOne'>Soy un estudiante resolutivo y proactivo que le gusta aprender cada vez más sobre el mundo del software, enfocandome en ser Fullstack para tener mas conocimientos sobre la mayoria de campos que abarque la industria del software.</p>
+      <p className='textSecond'>ACTUALMENTE ESTOY REALIZANDO UN TECNOLOGO SOBRE DESARROLLO DE SOFTWARE Y A LA MISMA VEZ APRENDIENDO MAS MEDIANTE CURSOS.</p>
+      <div className='textThreeAndTextFour'>
+        <p className='textThree'>Si quieres ponerte en contacto conmigo escribeme en</p>
+        <p className='textFour'>sergioanjaci@gmail.com</p>
+      </div>
+      </div>
+      </div>
       </section>
     </Main>
     </>
